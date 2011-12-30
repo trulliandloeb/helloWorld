17cb090a2b4f3d291aae155d67399fd0a0ea6b3a
trulliandloeb <trulliandloeb@hotmail.com>
2011-12-30 22:31:19 +0800

    add filter function

--- ReadLines/ReadLines/Program.cs
+++ ReadLines/ReadLines/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace ReadLines
 {
@@ -9,6 +10,15 @@ namespace ReadLines
     {
         static void Main(string[] args)
         {
+            var path = args[0];
+            var keyword = args[1];
+            var lines = File.ReadLines(path);
+            var result = lines.Where(line => line.Contains(keyword)).ToArray();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
         }
     }
 }
